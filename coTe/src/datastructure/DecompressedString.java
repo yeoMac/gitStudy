0e8cbf3f3f8902771e0c36f5b8d705bc1aeb6924
chuyeo <cntjdgus123@naver.com>
2025-02-27 23:47:58 +0900

    "문자열 압축 순서 문제 푸는 중"

---
+++ coTe/src/datastructure/DecompressedString.java
@@ -0,0 +1,31 @@
+package datastructure;
+
+import java.util.Stack;
+
+public class DecompressedString {
+
+    public static void main(String[] args) {
+        String s = "3(a2(b))ef";
+        String result = DecompressedS(s);
+        System.out.println(result);
+    }
+
+    private static String DecompressedS(String s) {
+        Stack<String> stack = new Stack<>();
+        String answer = "";
+
+        for (char c : s.toCharArray()) {
+            if (c==')') {
+                String a = "";
+                while (!stack.isEmpty() && stack.peek().charAt(0)=='(') {
+                    String pop = stack.pop();
+                    a = a + pop;
+
+                }
+                if ()
+            }
+            stack.push(String.valueOf(c));
+        }
+    }
+
+}
